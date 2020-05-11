1dc202453d11af5a8aa955cf7e914ade4cdc95e0
JaysonCheng <jinsengcheng@gmail.com>
2020-05-11 22:50:08 +0000

    added header file (navbar) and footer file with landing page and main page (item)

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-//Secondhand 
+var express = require("express");
+var app = express();
+
+
+
+
+
+app.set("view engine", "ejs");
+
+// ROOT ROUTE
+// LANDING PAGE
+app.get("/", function(req, res){
+	res.render("landing");
+});
+
+
+// ITEMS ROUTE
+app.get("/items", function(req, res){
+	res.render("items");
+});
+
+
+app.listen(process.env.PORT || 5000, process.env.IP, function(){
+    console.log("SECONDHAND IS RUNNING!");
+});
